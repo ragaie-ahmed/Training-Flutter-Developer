7d6bf8234653acc0b39e80084c5f0ff23b1c2579
ragaie-ahmed <ragaaiahmed18@gmail.com>
2026-01-27 16:37:25 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/tharadtech/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tharadtech
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:tharadtech/my_app.dart';
+
+void main(){
+  runApp(MyApp());
+}
